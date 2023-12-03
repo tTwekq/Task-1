e9b6f503759f45b59cf5b5ce0d1e61630a931dbc
tTwekq <150682119+tTwekq@users.noreply.github.com>
2023-12-03 23:48:14 +0200

    Add files via upload

---
+++ масив.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+
+void rearrangeArray(int A[], int size) {
+    int dodatniCslhi = 0;
+
+    
+    for (int i = 0; i < size; ++i) {
+        
+        if (A[i] > 0) {
+            swap(A[i], A[dodatniCslhi]);
+            dodatniCslhi++;
+        }
+    }
+}
+
+int main() {
+    setlocale(0, ".1251");
+
+    int A[]{ 8, -5, 10, -2 };
+    int size = sizeof(A) / sizeof(int);
+
+    unordered_set<int> uniqueNumbers;
+
+    for (int i = 0; i < size; i++) {
+        uniqueNumbers.insert(A[i]);
+    }
+
+    cout << "Кількість різних чисел: " << uniqueNumbers.size() << endl;
+
+    
+    rearrangeArray(A, size);
+
+    
+    for (int i = 0; i < size; ++i) {
+        cout << A[i] << endl;
+    }
+
+    return 0;
+}
